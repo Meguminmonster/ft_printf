ae37d0a59a29af5895210251eec0d8d4a2dab5f4
Meguminmonster <114344458+Meguminmonster@users.noreply.github.com>
2025-11-12 13:58:20 +0100

    Add files via upload

---
+++ printf/ft_puthex_pf.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_puthex_pf.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jpedra-v <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/11 15:11:10 by jpedra-v          #+#    #+#             */
+/*   Updated: 2025/11/11 15:11:23 by jpedra-v         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+void	ft_puthex_pf(unsigned int num, size_t *counter, char *base)
+{
+	char	*str;
+
+	str = ft_aux_pf(num, base);
+	ft_putstr_pf(str, counter);
+	free(str);
+}
