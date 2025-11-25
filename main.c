56d21e94c75899689a8d79d3891b8d6d2bb5590b
Meguminmonster <114344458+Meguminmonster@users.noreply.github.com>
2025-11-25 12:00:13 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include "ft_printf.h"
+#include <stdio.h>
 
-int     main()
+int  main()
 {
-  printf("%d\n", ft_printf("hola %sabc%dabc%X\n", "mundo", 123, 15))
+  ft_printf("hola %sabc%dabc%X\n", "mundo", 123, 15);
+  printf("hola %sabc%dabc%X\n", "mundo", 123, 15);
+  return (0);
 }
