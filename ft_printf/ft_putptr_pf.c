704061264be741a5c9dc3ce706e02f4f153ff84d
Meguminmonster <114344458+Meguminmonster@users.noreply.github.com>
2025-12-06 13:40:38 +0100

    Add files via upload

---
+++ ft_printf/ft_putptr_pf.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putptr_pf.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jpedra-v <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/11 15:12:09 by jpedra-v          #+#    #+#             */
+/*   Updated: 2025/11/11 15:12:22 by jpedra-v         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+void	ft_putptr_pf(void *ptr, size_t *counter)
+{
+	char			*str;
+	unsigned long	ptr_address;
+
+	if (ptr == NULL)
+	{
+		ft_putstr_pf("(nil)", counter);
+		return ;
+	}
+	ptr_address = (unsigned long)ptr;
+	ft_putstr_pf("0x", counter);
+	str = ft_aux_pf(ptr_address, HEX_LOW_BASE);
+	ft_putstr_pf(str, counter);
+	free(str);
+}
